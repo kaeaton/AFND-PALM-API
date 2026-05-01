84e2a5a74fb39428f1eb1194962439ef68235399
Katrina Eaton <katrina.eaton@ucsf.edu>
2026-05-01 13:23:03 -0700

    Image appears in the browser

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
 import subprocess
 ***REMOVED***
-
 import rpy2.robjects as ro
 from rpy2.robjects.packages import importr
 app = FastAPI(***REMOVED***
@@ -19,26 +19,18 @@ async def say_hello(name: str***REMOVED***:
 @app.get("/palm"***REMOVED***
 async def palm(allele: str***REMOVED***:
 
-     with ro.default_converter.context(***REMOVED***:
+    # Allows rpy2 to find its config settings even in a thread
+    with ro.default_converter.context(***REMOVED***:
+
+        # Load SSHAARP and the Solberg dataset
         sshaarp = importr('SSHAARP'***REMOVED***
         solberg_dataset = ro.r("SSHAARP::solberg_dataset"***REMOVED***
-    # palm = importr('PALM'***REMOVED***
 
-        image = sshaarp.PALM(allele, variantType="allele", filename=solberg_dataset, resolution = 500***REMOVED***
-    # Define the command to run the R script
-    # command = 'Rscript'
-    # path_to_script = 'R_pkgs/SSHAARP-AFND/SSHARP_functions_script.R::PALM("DRB1*01:01", variantType="allele", color=F, mask = T, filename=SSHAARP::solberg_dataset, resolution = 480***REMOVED***'
-    #
-    # # Optional: Add command-line arguments to pass to R
-    # args = 'PALM("DRB1*01:01", variantType="allele", mask = T, filename=SSHAARP::solberg_dataset, resolution = 500***REMOVED***'
-    #
-    # # Build and execute the command
-    # cmd = [command, path_to_script] #+ args
-    # result = subprocess.check_output(cmd, universal_newlines=True***REMOVED***
-    #
-    # # PALM("DRB1*01:01", variantType="allele", color=F, mask = T, filename=SSHAARP::solberg_dataset, resolution = 480***REMOVED***
+        # Generate the image
+        sshaarp.PALM(allele, variantType="allele", filename=solberg_dataset, resolution = 500***REMOVED***
 
-        print(image***REMOVED***
-        filename = allele.replace(':', '/'***REMOVED*** + '.jpg'
+        # filename = allele.replace(':', '/'***REMOVED*** + '.jpg'
+        filename = allele + '.jpg'
         file = Path(".", filename***REMOVED***
-        return file
+
+        return FileResponse(file***REMOVED***
