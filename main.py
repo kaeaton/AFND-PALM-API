de28de3329f4fceea332123fa82dbfbfb1d2a748
Katrina Eaton <katrina.eaton@ucsf.edu>
2026-06-22 11:25:39 -0700

    Move db password to .env

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
 import subprocess
+import os
+from dotenv import load_dotenv
 from pathlib import Path
 import rpy2.robjects as ro
 from rpy2.robjects.packages import importr
+from sqlalchemy import create_engine
+from sqlalchemy.engine import URL
 app = FastAPI()
 
 
@@ -18,6 +22,22 @@ async def say_hello(name: str):
 
 @app.get("/palm")
 async def palm(allele: str):
+
+    # load secrets from .env file
+    load_dotenv()
+    database_password = os.getenv("DB_PASSWORD")
+
+    connection_url = URL.create(
+        "mssql+mssql-python",
+        username="steve",
+        password=database_password, # Special characters are automatically escaped
+        host="74.208.198.44",
+        port=1433,
+        database="allel002",
+        query={"driver": "ODBC Driver 18 for SQL Server"}
+    )
+
+    engine = create_engine(connection_url)
 
     # Allows rpy2 to find its config settings even in a thread
     with ro.default_converter.context():
