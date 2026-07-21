3fe82b68b3119ff586c3c1dca64dc94d5f783c2c
Katrina Eaton <katrina.eaton@ucsf.edu>
2026-07-21 08:56:16 -0700

    files restored

--- main.py
+++ main.py
@@ -1,67 +1,62 @@
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
 import subprocess
-***REMOVED***
-***REMOVED***
-***REMOVED***
+from pathlib import Path
+from dotenv import load_dotenv
+import os
 import rpy2.robjects as ro
 from rpy2.robjects.packages import importr
-***REMOVED***
-***REMOVED***
-***REMOVED***
+from sqlalchemy import create_engine
+from sqlalchemy.engine import URL
+from sqlalchemy import text
+import pandas as pd
 
+app = FastAPI()
 
-
-app = FastAPI(***REMOVED***
-
-
-@app.get("/"***REMOVED***
-async def root(***REMOVED***:
+@app.get("/")
+async def root():
     return {"message": "Hello World"}
 
-
-@app.get("/hello/{name}"***REMOVED***
-async def say_hello(name: str***REMOVED***:
+@app.get("/hello/{name}")
+async def say_hello(name: str):
     return {"message": f"Hello {name}"}
 
-@app.get("/palm"***REMOVED***
-async def palm(allele: str***REMOVED***:
+@app.get("/palm")
+async def palm(allele: str):
+    load_dotenv()
+    database_password = os.getenv("DB_PASSWORD")
 
-    ***REMOVED***
-    ***REMOVED***
-    ***REMOVED***
+    connection_url = URL.create(
+               "mssql+pyodbc",
+               username="steve",
+               password=database_password, # Special characters are automatically escaped
+               host="74.208.198.44",
+               port=1433,
+               database="allel002",
+               query={"driver": "ODBC Driver 18 for SQL Server", "TrustServerCertificate": "yes"}
+               )
 
-    ***REMOVED***
-    ***REMOVED***
-    ***REMOVED***
-    ***REMOVED***
-    ***REMOVED***
-    ***REMOVED***
-    ***REMOVED***
-    ***REMOVED***
-    ***REMOVED***
-
-    engine = create_engine(connection_url***REMOVED***
+    engine = create_engine(connection_url)
 
     # Check image_list.tsv
-    image_list = Path("./image_list.tsv"***REMOVED***
+    image_list = Path("./image_list.tsv")
 
 
 
     # Allows rpy2 to find its config settings even in a thread
-    with ro.default_converter.context(***REMOVED***:
+    with ro.default_converter.context():
 
         # Load SSHAARP and the Solberg dataset
-        sshaarp = importr('SSHAARP'***REMOVED***
-        print("imported sshaarp"***REMOVED***
-        solberg_dataset = ro.r("SSHAARP::solberg_dataset"***REMOVED***
-        print("found the solberg dataset"***REMOVED***
+        sshaarp = importr('SSHAARP')
+        print("imported sshaarp")
+        solberg_dataset = ro.r("SSHAARP::solberg_dataset")
+        print("found the solberg dataset")
 
         # Generate the image
-        sshaarp.PALM(allele, variantType="allele", filename=solberg_dataset, resolution = 500***REMOVED***
+        sshaarp.PALM(allele, variantType="allele", filename=solberg_dataset, resolution = 500)
 
-        # filename = allele.replace(':', '/'***REMOVED*** + '.jpg'
+        # filename = allele.replace(':', '/' + '.jpg'
         filename = allele + '.jpg'
-        file = Path(".", filename***REMOVED***
+        file = Path(".", filename)
 
-        return FileResponse(file***REMOVED***
+        return FileResponse(file)
